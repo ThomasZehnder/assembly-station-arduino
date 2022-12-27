9d69e8a9b2913c23d92ae068f1b9643f5e15e1ab
Thomas Zehnder <thomas.zehnder@zhs.ch>
2022-12-27 16:11:10 +0100

    Keys added key pressed send mqtt message

--- src/main.cpp
+++ src/main.cpp
@@ -73,6 +73,19 @@ void loop()
         ws2812Loop();
     }
 
+    if (keyPressed(0))
+    {
+        mqttPublishLong("assembly-001/key-1", keyPressedCounter(0));
+    }
+    if (keyPressed(1))
+    {
+        mqttPublishLong("assembly-001/key-2", keyPressedCounter(1));
+    }
+    if (keyPressed(2))
+    {
+        mqttPublishLong("assembly-001/key-3", keyPressedCounter(2));
+    }
+
     mqttLoop();
     httpLoop();
 }
