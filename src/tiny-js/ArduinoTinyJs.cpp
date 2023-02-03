ca67aed46c25480d797c655a499eedea38cc06b0
Thomas Zehnder <thomas.zehnder@zhs.ch>
2023-02-03 21:17:52 +0100

    TinyJS transformed to class

--- src/tiny-js/ArduinoTinyJs.cpp
+++ src/tiny-js/ArduinoTinyJs.cpp
@@ -1,33 +1,5 @@
 /*
- * TinyJS
- *
- * A single-file Javascript-alike engine
- *
- * Authored By Gordon Williams <gw@pur3.co.uk>
- *
- * Copyright (C) 2009 Pur3 Ltd
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
-
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
-
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-/*
- * This is a simple program showing how to use TinyJS
+ * Arduino Interface with TinyJS
  */
 
 #include "TinyJS.h"
@@ -36,15 +8,23 @@
 // #include <stdio.h>
 #include <Arduino.h>
 
+#include "ArduinoTinyJs.h"
+
+
+ArduinoTinyJs tinyJs;
+
 // const char *code = "var a = 5; if (a==5) a=4; else a=3;";
 // const char *code = "{ var a = 4; var b = 1; while (a>0) { b = b * 2; a = a - 1; } var c = 5; }";
 // const char *code = "{ var b = 1; for (var i=0;i<4;i=i+1) b = b * 2; }";
-const char *code = "function myfunc(x, y) { return x + y; } var a = myfunc(1,2); print(a);";
+// const char *code = "function myfunc(x, y) { return x + y; } var a = myfunc(1,2); print(a);";
+
+
 
 void js_print(CScriptVar *v, void *userdata)
 {
   Serial.print(">> ");
   Serial.println(v->getParameter("text")->getString().c_str());
+
 }
 
 /*
@@ -54,14 +34,11 @@ void js_dump(CScriptVar *v, void *userdata) {
 }
 */
 
-int loopCounter = 0;
 
-// create instance dynamic
-CTinyJS *js;
-
-void tinyJsSetup()
+void ArduinoTinyJs::setup()
 {
-  Serial.println("TinyJsSetup --> Start");
+  Serial.println("TinyJsSetup...Start");
+
   // create instance dynamic
   js = new CTinyJS();
 
@@ -77,6 +54,9 @@ void tinyJsSetup()
     js->execute("var x = 0; x++; print(x);");
     js->execute("var y = 0; function inc(i) { i++; return i;}");
     
+    //execute file content loaded
+    js->execute(initStr.c_str());
+    
   }
   catch (CScriptException *e)
   {
@@ -88,15 +68,14 @@ void tinyJsSetup()
     Serial.println("INIT ERROR: Catch all...");
   }
 
-  Serial.println("TinyJsSetup --> End");
+  Serial.println("TinyJsSetup...End");
 }
 
-void tinyJsLoop()
+void ArduinoTinyJs::loop()
 {
   loopCounter++;
   Serial.print("tinyJsLoop Start: ");
   Serial.println(loopCounter);
-
   // like _CYCLIC
   try
   {
@@ -109,6 +88,9 @@ void tinyJsLoop()
     //js->execute("var x = 0; x++; print(x);");
     js->execute("x++; print('x='+x);");
     js->execute("y = inc(y); print('y='+y);");
+
+    //execute file content loaded
+    js->execute(cyclicStr.c_str());
 
     //
     //js->execute("unknown_function();");
@@ -127,8 +109,22 @@ void tinyJsLoop()
   //delete js;
 }
 
-void tinyJsTeardown()
+void ArduinoTinyJs::tearDown()
 {
   //like _EXIT
   delete js;
+  js = NULL;
+}
+
+// constructor
+ArduinoTinyJs::ArduinoTinyJs() {
+   setupCmd = false;
+   loopCmd = false;
+
+   printStr[0] = "constructor";
+   loopCounter = 0;
+
+   initStr = "print('init default');";
+   cyclicStr = "print('cyclic default');";
+   
 }
