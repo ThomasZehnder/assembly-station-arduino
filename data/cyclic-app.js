92a89f8988c4fa0195f02f103e99545a157b639c
Thomas Zehnder <thomas.zehnder@zhs.ch>
2023-02-05 16:13:48 +0100

    Math.rand() funtion works, JSON,stringify not.

--- data/cyclic-app.js
+++ data/cyclic-app.js
@@ -1,10 +1,17 @@
 // java script cyclic
 print('CYCLIC from File');
 
+
 x++;
 
-print('x='+x);
+print('x=' + x);
 
-y= inc(y);
+y = inc(y);
 
-print('y='+y);
+print('y=' + y);
+
+//not working, CRASH !!!
+//print(JSON.stringify({a:7}));
+
+print(Math.rand());
+print(Math.randInt(2, 10));
