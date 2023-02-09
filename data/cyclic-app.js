dc41bec0a55a9b71169f3d4ed6d2bcb67a54fbaf
Thomas Zehnder <thomas.zehnder@zhs.ch>
2023-02-09 20:44:10 +0100

    cyclic_app extended

--- data/cyclic-app.js
+++ data/cyclic-app.js
@@ -1,53 +1,60 @@
 // java script cyclic
-if (1 === 0) {
+if (1 === 1) {
     print('CYCLIC from File');
-
-    print(Esp.millis());
+    print("millis=" + Esp.millis());
 
     function dec(x) {
-        x--;
+       x--;
         return x;
     }
-    x++;
-
+    //x++;
     print('x=' + x);
 
     y = inc(y);
-
     print('y=' + y);
 
-    //not working, CRASH !!!
-    //print(JSON.stringify({a:7}));
     var r = Math.rand();
     print(r);
     print(Math.randInt(2, 10));
 }
 
-//fom Math package 
+//from Math package
 
 //print("sin of " + r + "=" + Math.sin(r));
+
 if (x >= 20) {
     Esp.clearLed();
     x = 0;
 }
-var c = "on";//white
-if (x >= 2) c = "green";
-if (x >= 4) c = "red";
-if (x >= 6) c = "yellow";
-if (x >= 8) c = "off";//black
-if (x >= 10) c = "#FF0000";//red
-if (x >= 12) c = "#00FF00";//green
-if (x >= 14) c = "#FFFFFF";//white full brightness
-if (x >= 16) c = "x";//white 50% brightness to showerror
-print(x);
-Esp.setLed(x % 4, c);
-x++;
 
+var c = "on";//white
+
+if (x >= 16) c = "x";//white 50% brightness to showerror
+else if (x >= 14) c = "#FFFFFF";//white full brightness
+else if (x >= 12) c = "#00FF00";//green
+else if (x >= 10) c = "#FF0000";//red
+else if (x >= 8) c = "off";//black
+else if (x >= 6) c = "yellow";
+else if (x >= 4) c = "red";
+else if (x >= 2) c = "green";
+
+print(x + ":" + x % 4 + ":" + c);
+
+Esp.setLed(x % 4, c);
+
+x++;
 print("Key[0] = " + Esp.getKey(0));
 print("Key[0]Counter = " + Esp.getKeyCounter(0));
+
 if (Esp.getKey(0)) {
     Esp.setLed(0, "#FFFFFF");
 }
 else {
     Esp.setLed(0, "off");
 }
+
+
+
+
+
+
